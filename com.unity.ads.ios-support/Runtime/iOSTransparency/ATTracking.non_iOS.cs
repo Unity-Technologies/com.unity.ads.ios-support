27c0f048fd566e792101a58f01b027d49c79a3a2
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2021-12-01 19:57:38 +0200

    Making usage easier
    Added AATracking class in two variants:
    1. iOS with implementations,
    2. non-iOS for supporting compilation for all other platforms.
    
    AATracking has easy to use Property called "bool SupportediOSVersion", it returns result of iOS version validation.
    Also there is function "RequestAuthorizationTracking" with better callback implementation. Replaced raw int values to enum which tells more about the status in callback.

---
+++ com.unity.ads.ios-support/Runtime/iOSTransparency/ATTracking.non_iOS.cs
@@ -0,0 +1,23 @@
+﻿#if !UNITY_IOS
+
+using System;
+
+namespace Unity.Advertisement.IosSupport
+{
+    using TrackingStatus = ATTrackingStatusBinding.AuthorizationTrackingStatus;
+
+    public static class ATTracking
+    {
+        public static bool SupportediOSVersion => false;
+        public static bool IsTrackingAccepted()
+        {
+            return false;
+        }
+        public static bool RequestAuthorizationTracking(Action<TrackingStatus> callback = null)
+        {
+            // Do nothing
+            return false;
+        }
+    }
+}
+#endif
